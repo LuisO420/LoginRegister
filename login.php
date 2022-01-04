a7e75884d539a812bc601bbb98ddc91a66efb13e
LuisO420 <72115916+LuisO420@users.noreply.github.com>
2022-01-03 23:53:32 -0700

    Update login.php

--- login.php
+++ login.php
@@ -11,8 +11,8 @@ include("login-function.php");
 <body>
     <form class="form" method="post" name="login">
         <h1 class="login-title">Login</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
-        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="text" class="login-input" name="username" placeholder="Nombre de usuario" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Contraseña"/>
         <input type="submit" value="Login" name="submit" class="login-button"/>
         <p class="link"><a href="registration.php">New Registration</a></p>
   </form>
